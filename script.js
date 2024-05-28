9ed573a6086830174802eb8a70704af59ba72572
Monika Joshi <joshimonika20may@gmail.com>
2024-05-28 20:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+table{
+	border-collapse: collapse;
+	width: 100%;
+	height: 100%;
+}
+table, th, td{
+	border-color: black;
+	border: 1px;
+	border-style: solid;
+}
+td{
+	text-align: right;
+}
+th{
+	text-align: left;
+}
+#eg1{
+	text-align: left;
+}
